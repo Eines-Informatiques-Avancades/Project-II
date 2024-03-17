c81d8ad30b906ca0779b28856b5704193fd06782
guillem_ares <guillemares4@gmail.com>
2024-03-17 19:21:25 +0100

    Primer commit amb PBC i inicialitzacio

---
+++ pbc_EIA.f90
@@ -0,0 +1,25 @@
+module pbc_mod
+    implicit none
+contains
+
+    subroutine PBC(position, L, N)
+        double precision, dimension(:,:), intent(inout) :: position
+        double precision, intent(in) :: L
+        integer :: i, j, M, N
+
+
+        N = size(position(:,1))
+        M = size(position(1,:))
+
+        do i = 1, N
+            do j = 1, M
+                if (position(i,j) > L) then
+                    position(i,j) = position(i,j) - L
+                else if (position(i,j) < 0) then
+                    position(i,j) = position(i,j) + L
+                end if
+            end do
+        end do
+    end subroutine PBC
+
+end module pbc_mod
