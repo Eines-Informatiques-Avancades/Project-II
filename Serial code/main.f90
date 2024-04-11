bb81b187fb2c64aca4ecb0e7040ac27ad6cc5702
psierrva <psierrva26@alumnes.ub.edu>
2024-04-11 02:35:01 +0200

    Thermal bath implementation

--- Serial code/main.f90
+++ Serial code/main.f90
@@ -8,23 +8,24 @@ program main_simulation
     implicit none
     real*8, allocatable, dimension(:,:) :: positions, velocities 
     integer :: N,n_steps,n_save_pos
-    real*8 :: L,temperature,dt,cutoff,epsilon
+    real*8 :: L,temperature,dt,cutoff,epsilon,sigma,nu
     character (len=500) :: simulation_name
     ! reads input file
     print*, 'START OF SIMULATION.'
     call read_parameters("parameters.nml", dt, N, n_steps, n_save_pos, L,&
-    simulation_name, temperature, cutoff, epsilon)
+    simulation_name, temperature, epsilon, cutoff, nu)
     temperature = temperature/epsilon
+    sigma = dsqrt(temperature)
     print*, 'Parameters of the simulation loaded.'
     ! allocates memory
     allocate(positions(N,3),velocities(N,3))
     print*, 'Needed arrays allocated.'
     ! initialize system
     call initial_positions(N, L, positions)
-    call vel_bimodal(temperature,velocities)
+    call initial_velocities(N, temperature, velocities)
     print*, 'System initialized.'
     ! simulation loop
-    call main_loop(n_steps,n_save_pos, dt, L, cutoff, positions, velocities)
+    call main_loop(n_steps,n_save_pos, dt, L, sigma, nu, cutoff, positions, velocities)
     ! deallocates memory
     deallocate(positions,velocities)
     write(*,'(A)') "END OF SIMULATION."
