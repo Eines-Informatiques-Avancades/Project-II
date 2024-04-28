cc44d81d15651cf1b843bee908918015978596fd
guillem_ares <guillemares4@gmail.com>
2024-04-28 17:06:18 +0200

    parallelling new branch

--- Parallel code/main.f90
+++ Parallel code/main.f90
@@ -10,9 +10,11 @@ program main_simulation
     !include 'mpif.h'
     implicit none
     real*8, allocatable, dimension(:,:) :: positions, velocities 
-    integer :: N,n_steps,n_save_pos,nproc,ierror,iproc
+    integer :: N,n_steps,n_save_pos,nproc,ierror,iproc,i
     real*8 :: L,temperature,dt,cutoff,vcutoff,epsilon,sigma,nu
     character (len=500) :: simulation_name
+    real*8, allocatable, dimension(:,:) :: local_positions, all_positions
+
 
     call mpi_init(ierror)
     call mpi_comm_rank(MPI_COMM_WORLD,iproc,ierror)
@@ -44,15 +46,42 @@ program main_simulation
     ! CHARACTER broadcasting
     call MPI_Bcast(simulation_name, 500, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierror)
     ! allocates memory
-    allocate(positions(N,3),velocities(N,3))
+    allocate(velocities(N,3))
     print*, 'Needed arrays allocated.'
-    ! initialize system
+    allocate(local_positions(N/nproc,3))
+    call initial_positions(N, L, local_positions, iproc)
+
+    ! if (iproc == 0) then
+    ! print*, local_positions
+    ! endif
+
+    if (iproc == 0) then
+        allocate(all_positions(N,3))
+    endif
+
+    ! call MPI_ALLGATHER(Nsub, 1, MPI_INTEGER, gather_counts, 1, MPI_INTEGER, comm, ierror)
+    ! gather_displs(1) = 0
+    ! do j = 2, nproc
+    !     gather_displs(j) = gather_displs(j - 1) + gather_counts(j - 1)
+    ! do j=1,3
+    !     call MPI_ALLGATHERV(positions(imin:imax, j), Nsub, MPI_DOUBLE_PRECISION, &
+    !     positions(:,j), gather_counts, gather_displs, MPI_DOUBLE_PRECISION, &
+    !     comm, ierror)
+    ! enddo
+
+    if (iproc == 0) then
+        print*, 'All positions gathered.'
+        print*, all_positions
+        deallocate(all_positions)
+    endif
+
+
+
     if (iproc==0) then
-        call initial_positions(N, L, positions)
+        ! call initial_positions(N, L, positions)
         call initial_velocities(N, temperature, velocities)
         print*, 'System initialized.'
     endif
-    call MPI_Bcast(positions,   N*3, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierror)
     call MPI_Bcast(velocities,  N*3, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierror)
 
 
@@ -60,8 +89,9 @@ program main_simulation
     call main_loop(MPI_COMM_WORLD,iproc,n_steps,n_save_pos, dt, L, sigma, nu, nproc, cutoff, vcutoff, positions, velocities)
     ! deallocates memory
     if (iproc==0) then
-        deallocate(positions,velocities)
+        deallocate(velocities)
         write(*,'(A)') "END OF SIMULATION."
     endif
     call mpi_finalize(ierror)
+    ! deallocate(subsystems)
 end program main_simulation
--- Parallel code/readers.f90
+++ Parallel code/readers.f90
@@ -4,24 +4,24 @@ contains
 ! call read_parameters("parameters.nml", dt, N, n_steps, n_save_pos, L,&
 ! simulation_name, temperature, epsilon, cutoff, nu, nproc)
 subroutine read_parameters(param_file, dt, n_particles, n_steps, n_save_pos, L,&
-    simulation_name, temperature,epsilon,cutoff,nu,nproc)
+    simulation_name, temperature,epsilon,cutoff, vcutoff, nu,nproc)
     implicit none
     ! Subroutine to read the simulation parameters from a an nml list
     !
     ! INPUT:
     !   param_file : The path of the parameter file 
     character(len=*), intent(in)      :: param_file
-    real*8, intent(inout)               :: dt, temperature, L, cutoff,epsilon,nu
+    real*8, intent(inout)               :: dt, temperature, L, cutoff,epsilon,nu, vcutoff
     integer, intent(inout)              :: n_particles, n_steps, n_save_pos,nproc
     character(len=500), intent(inout)   :: simulation_name
 
     integer                              :: unit_nr, iost
     character(len=1024)                  :: msg
     ! Namelist variables
-    
+
 
     namelist /PARAMS/ dt, n_particles, n_save_pos, L, temperature,&
-    n_steps, simulation_name, epsilon,cutoff,nu,nproc
+    n_steps, simulation_name, epsilon,cutoff,vcutoff, nu,nproc
 
     ! Reading namelist
     open(newunit=unit_nr, file=trim(param_file), access='sequential', &
